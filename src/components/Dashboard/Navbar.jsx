a419f55e213c73e9e77f2350a73d171aaaaf6262
nathan-renner <50720516+nathan-renner@users.noreply.github.com>
2021-01-31 11:12:01 -0500

    added stat tooltips for dashboard navbar

--- src/components/Dashboard/Navbar.jsx
+++ src/components/Dashboard/Navbar.jsx
@@ -12,15 +12,17 @@ const Navbar = () => {
   const [avatar] = useState(null);
 
   return (
-    <div className="navbar-container">
+    <div className="navbar-container-dashboard">
       <div className="navbar">
         <div>
           <h2>DASHBOARD</h2>
         </div>
         <div className="stats-container">
           <div className="stat-container">
-            <BriefcaseIcon height={30} width={30} />
-            <h2 className="stat-text text-primary">2</h2>
+            <div className="stat">
+              <BriefcaseIcon height={30} width={30} />
+              <h2 className="stat-text text-primary">2</h2>
+            </div>
             <div className="stat-tooltip">
               <h3 className="stat-title">Completed Jobs</h3>
               <p className="stat-description">
@@ -28,19 +30,41 @@ const Navbar = () => {
               </p>
               <div className="stat-num-container">
                 <BriefcaseIcon height={80} width={80} />
-                <h1 class="stat-number">2</h1>
+                <h1 class="stat-number text-primary">2</h1>
               </div>
             </div>
           </div>
           <div className="stat-container">
-            <PencilIcon height={30} width={30} />
-            <h2 className="stat-text text-purple">2</h2>
-            <div className="stat-tooltip"></div>
+            <div className="stat">
+              <PencilIcon height={30} width={30} />
+              <h2 className="stat-text text-purple">2</h2>
+            </div>
+            <div className="stat-tooltip">
+              <h3 className="stat-title">Applications</h3>
+              <p className="stat-description">
+                Fill out applications to increase your chance at full employment{" "}
+              </p>
+              <div className="stat-num-container">
+                <PencilIcon height={80} width={80} />
+                <h1 class="stat-number text-purple">2</h1>
+              </div>
+            </div>
           </div>
           <div className="stat-container">
-            <ClipboardIcon height={30} width={30} />
-            <h2 className="stat-text text-secondary">2</h2>
-            <div className="stat-tooltip"></div>
+            <div className="stat">
+              <ClipboardIcon height={30} width={30} />
+              <h2 className="stat-text text-secondary">2</h2>
+            </div>
+            <div className="stat-tooltip">
+              <h3 className="stat-title">References</h3>
+              <p className="stat-description">
+                Build your portfolio with good employer reviews
+              </p>
+              <div className="stat-num-container">
+                <ClipboardIcon height={80} width={80} />
+                <h1 class="stat-number text-secondary">2</h1>
+              </div>
+            </div>
           </div>
         </div>
         <div className="right-container">
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import Home from "./../views/dashboard/Home";
+import Sidebar from "./../components/Dashboard/Sidebar";
+import Navbar from "./../components/Dashboard/Navbar";
 
 const path = "/dashboard";
 
 const Dashboard = () => {
   return (
-    <Switch>
-      <Route exact path={`${path}`} component={Home} />
-      <Redirect to="/not-found" />
-    </Switch>
+    <div className="dashboard">
+      <Sidebar />
+      <div className="main-container">
+        <Navbar />
+        <div className="content-container">
+          <Switch>
+            <Route exact path={`${path}`} component={Home} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </div>
+      </div>
+    </div>
   );
 };
 
