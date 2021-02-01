dfd39767333d4797f74358bab5a38cf04b44f746
nathan-renner <50720516+nathan-renner@users.noreply.github.com>
2021-02-01 09:16:40 -0500

    added button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Button({
+  label = "",
+  color = "primary",
+  textColor = "white",
+  outline = false,
+  textStyle,
+  buttonStyle,
+  onClick,
+  ...otherProps
+}) {
+  return (
+    <div
+      className={`button btn-${color} ${outline ? "outline" : null}`}
+      style={buttonStyle}
+      onClick={onClick}
+      {...otherProps}
+    >
+      <p className={`btn-text text-${textColor}`} style={textStyle}>
+        {label}
+      </p>
+    </div>
+  );
+}
+
+export default Button;
